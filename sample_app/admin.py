1151594003128700d0c7d02bbc64088f523cbdfc
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-03 22:18:03 -0500

    filters

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -30,13 +30,20 @@ class QuestionAdmin(admin.ModelAdmin):
 
     list_display = ('question_text','ref_author', 'pub_date', 'created_date', 'updated_date')
     list_display_links = ('ref_author',)
+    list_editable = ('question_text',)
     date_hierarchy = 'pub_date'
     save_on_top = True
+    list_filter = ('ref_author', 'pub_date')
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'choice_text', 'votes', 'created_date', 'updated_date')
+    list_filter = ('question__ref_author',)
+    search_fields = ('choice_text','question__question_text', 'question__ref_author__name')
 
 
 admin.site.register(Author,AuthorAdmin)
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+admin.site.register(Choice, ChoiceAdmin)
 
 
 # @admin.register(Author)
