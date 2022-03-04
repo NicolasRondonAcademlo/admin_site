8272a697fe9f5d5781706411d7ff6125fb3cb192
Academlo <academlo@MacBook-Pro-de-nicolas.local>
2022-03-03 21:53:59 -0500

    complex

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -9,7 +9,35 @@ class AuthorAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'created_date', 'updated_date')
 
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question information", {
+            'fields': ('question_text',)
+        }),
+        (
+            "Date", {
+                "fields": ("pub_date",)
+            }
+        ),
+        ('The author', {
+            'classes': ('collapse',),
+            'fields': ('ref_author',),
+        })
+    ]
+
+    list_display = ('question_text','ref_author', 'pub_date', 'created_date', 'updated_date')
+    list_display_links = ('question_text','ref_author',)
 
 admin.site.register(Author,AuthorAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
+
+# @admin.register(Author)
+# class AuthorAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['name']})
+#     ]
+#
+#     list_display = ('name', 'created_date', 'updated_date')
+# En caso de usar esta manera debemos elminiar la linea admin.site.register(Author,AuthorAdmin)
